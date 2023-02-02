c16cbf96d37d9f3c36806adbd5b058e1b0932797
Vojtěch Sázel <wojta@users.noreply.github.com>
2023-02-02 01:03:21 +0100

    wip

---
+++ drivers/node-sqlite3-driver/src/main/kotlin/cz/sazel/sqldelight/node/sqlite3/SQLite3Cursor.kt
@@ -0,0 +1,29 @@
+package cz.sazel.sqldelight.node.sqlite3
+
+import app.cash.sqldelight.db.SqlCursor
+
+internal class SQLite3Cursor : SqlCursor {
+    override fun next(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getString(index: Int): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLong(index: Int): Long? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBytes(index: Int): ByteArray? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDouble(index: Int): Double? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBoolean(index: Int): Boolean? {
+        TODO("Not yet implemented")
+    }
+}
--- drivers/node-sqlite3-driver/src/main/kotlin/cz/sazel/sqldelight/node/sqlite3/SQLite3Driver.kt
+++ drivers/node-sqlite3-driver/src/main/kotlin/cz/sazel/sqldelight/node/sqlite3/SQLite3Driver.kt
@@ -1,11 +1,32 @@
+package cz.sazel.sqldelight.node.sqlite3
+
 import app.cash.sqldelight.Query
 import app.cash.sqldelight.Transacter
 import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlCursor
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.db.SqlPreparedStatement
+import node.sqlite3.Sqlite3
 
 class SQLite3Driver : SqlDriver {
+    private val listeners = mutableMapOf<String, MutableSet<Query.Listener>>()
+    private var transaction: Transacter.Transaction? = null
+
+    private inner class Transaction(
+        override val enclosingTransaction: Transacter.Transaction?,
+    ) : Transacter.Transaction() {
+        override fun endTransaction(successful: Boolean): QueryResult<Unit> = QueryResult.AsyncValue {
+            if (enclosingTransaction == null) {
+                if (successful) {
+                    //worker.run("END TRANSACTION")
+                } else {
+                    //worker.run("ROLLBACK TRANSACTION")
+                }
+            }
+            transaction = enclosingTransaction
+        }
+    }
+
     override fun <R> executeQuery(identifier: Int?, sql: String, mapper: (SqlCursor) -> R, parameters: Int, binders: (SqlPreparedStatement.() -> Unit)?): QueryResult<R> {
         TODO("Not yet implemented")
     }
@@ -36,5 +57,13 @@ class SQLite3Driver : SqlDriver {
 
     override fun close() {
         TODO("Not yet implemented")
+    }
+
+    private fun Sqlite3.Statement.bind(parameters: Int, binders: (SqlPreparedStatement.() -> Unit)?) = binders?.let {
+        if (parameters > 0) {
+            val bound = SQLite3PreparedStatement(parameters)
+            binders(bound)
+            bind(bound.parameters)
+        }
     }
 }
